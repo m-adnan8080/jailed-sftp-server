9c279c3d2c4b2c66bf09aff4733bc0cbf37e4f95
Muhammad Adnan Khan <m.adnan80@gmail.com>
2025-12-15 18:15:49 +0500

    Update del_user.sh

--- del_user.sh
+++ del_user.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+SFTP_BASE="/var/sftp"
+
 if [ $# -lt 1 ]; then
     echo -e "Usage:  $0 [username]"
     exit 1
 else
     if id "$1" &> /dev/null; then
-        echo "Removing $1 sftp user and its directory /var/sftp/$1."
-        deluser --force $1
-	rm -rf /var/sftp/$1
+        echo "Removing $1 sftp user and its directory $SFTP_BASE/$1."
+        if mountpoint -q "$SFTP_BASE/$1/dev/log"; then
+            umount "$SFTP_BASE/$1/dev/log"
+        fi
+        userdel "$1"
+        rm -rf "$SFTP_BASE/$1"
+        echo "SFTP user $1 removed successfully."
     else
-        echo "$1 sftp user doesn't exists on server"
-        exit 1;
+        echo "$1 sftp user doesn't exist on server"
+        exit 1
     fi
 fi
